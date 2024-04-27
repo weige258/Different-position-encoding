52d201062010f882bf4261d663c5c2d8133d24df
weige258 <145858033+weige258@users.noreply.github.com>
2024-04-27 10:44:03 +0800

    Add files via upload

--- sin_cos.py
+++ sin_cos.py
@@ -1,10 +1,12 @@
 import torch
 import math
 def positional_encoding(tensor):
-    pe=torch.zeros(tensor.size()[0],tensor.size()[1])
-    pos_index=torch.arange(0,tensor.size(0))
-    div_term = torch.exp(torch.arange(0, tensor.size()[1],2) * (-math.log(10000.0)/tensor.size()[1]))
+    device=tensor.device
+    pe=torch.zeros(tensor.size()[0],tensor.size()[1],device=device)
+    pos_index=torch.arange(0,tensor.size(0),device=device)
+    div_term = torch.exp(torch.arange(0, tensor.size()[1],2,device=device) * (-math.log(10000.0)/tensor.size()[1]))
     pe[:, 0::2] = torch.sin(pos_index.unsqueeze(-1) / div_term)
     pe[:, 1::2] = torch.cos(pos_index.unsqueeze(-1) / div_term)
     tensor=tensor.to(dtype=torch.float)+pe
     return tensor
+
